4f3e8834d9a4a9369c9935addf1a0ff69d56bbe2
Nejc Vidmar <nejcviddmar@gmail.com>
2023-10-02 18:05:39 +0200

    push

--- frontEnd/vite.config.js
+++ frontEnd/vite.config.js
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-
-  base: "/nodeJS-chatAPP",
+  base: "nodeJS-chatAPP/frontEnd/",
   plugins: [react()],
 })
